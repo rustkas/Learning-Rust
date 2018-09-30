1c4ae000329b2d9fbe35c69c094a95d8e020cb57
Anatolij <java1cprog@yandex.ru>
2018-09-30 14:17:17 +0200

    Update main.rs

--- Chapter02/addstring-1/src/main.rs
+++ Chapter02/addstring-1/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let homeTeam = "Liverpool";
     let result = " beat ";
-    let awayTeam = "Manchester United";
-    let theString = homeTeam + result + awayTeam;
+    let awayTeam = "Manchester United
+    // illigal: binary operation `+` cannot be applied to type `&str`
+    // let theString = homeTeam + result + awayTeam;
 }
